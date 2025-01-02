e76e3dc6ff72d5c5f607987cb9337c928cabbc67
Jeevan <53844739+G1Grg@users.noreply.github.com>
2025-01-02 22:08:08 +1000

    updates on the @childView and @childContent

--- src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
-  imports: [ButtonComponent,ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-tickets.component.html',
-  styleUrl: './new-tickets.component.css'
+  styleUrl: './new-tickets.component.css',
 })
 export class NewTicketsComponent {
+  // @ViewChild('form') private form?:ElementRef<HTMLFormElement>;
 
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+
+  onSubmit(title: string, ticketText: string) {   
+    console.log('Entered value is: ' +title);
+    console.log('Entered ticket Text is: ' + ticketText);
+    this.form().nativeElement.reset();
+  
+  }
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,14 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  contentChild,
+  ContentChild,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,12 +16,29 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation:ViewEncapsulation.None,
-  host:{
-    class:'control'
-  }
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()',
+  },
 })
-export class ControlComponent {
+export class ControlComponent{
+  // Accessing class using @HostBinding
+  //  @HostBinding ('class') className = "control"
+
+
   label = input.required<string>();
-  
+  private el = inject(ElementRef);
+
+  // @ContentChild('input') private control?: ElementRef<
+  //   HTMLInputElement | HTMLTextAreaElement
+  // >;
+
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>> ('input');
+
+
+  onClick() {
+    console.log('clicked!@!!')
+    console.log(this.control());
+  }
 }
